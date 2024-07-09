d0398af2c92bbc236fff4e41824a8784a8a951a3
ynori123 <norisama1159@gmail.com>
2024-07-09 22:35:31 +0900

    initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <main className="h-screen w-screen">{children}</main></body>
     </html>
   );
 }
