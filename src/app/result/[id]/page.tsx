fc5ae7f77d6c30f0d657a7c940e0d43e25a7f6c7
ynori123 <norisama1159@gmail.com>
2024-07-12 16:04:11 +0900

    chore: Add buy button and Twitter share functionality to result page

--- src/app/result/[id]/page.tsx
+++ src/app/result/[id]/page.tsx
@@ -21,8 +21,8 @@ type PanelistType = {
 
 export default function Result() {
   const params = useParams<{ id: string }>()
-  const router = useRouter()
   const [oshi, setOshi] = useState<PanelistType | undefined>(undefined)
+  const [pending, setPending] = useState(false)
 
   useEffect(() => {
     const fetchPanelist = async () => {
@@ -37,6 +37,11 @@ export default function Result() {
     fetchPanelist()
   }, [params.id])
 
+  const handleOnClick = () => {
+    setPending(true)
+    handleBuy()
+    setPending(false)
+  }
   const handleBuy = async () => {
     const response = await fetch(`/api/panelists/${params.id}`, {
       method: 'POST'
@@ -44,13 +49,14 @@ export default function Result() {
     if (response.ok) window.open("https://froggy.smbcnikko.co.jp/promo/lp_account/ad/")
     else console.error('Error buying oshikabu')
   }
+  const twitterShareContext = `私の推し株は${oshi?.oshikabu.name}です！%0a${oshi?.oshikabu.companies}に注目しています！%0a%0a%0aあなたの推し株を診断します。株式投資への第一歩を踏み出しましょう！%0a`
   return (
     <Dialog open>
-      <DialogContent>
+      <DialogContent className='max-h-full'>
         <DialogHeader>
           <DialogTitle className='text-xl'>あなたの推し株: {oshi?.oshikabu.name}</DialogTitle>
         </DialogHeader>
-        <DialogDescription className='text-lg text-primary space-y-4'>
+        <DialogDescription className='text-lg text-primary space-y-4 max-h-full'>
           <div className='w-full flex justify-center'>
             {oshi
             ? <Image src={`/oshikabu/${oshi.oshikabuIndex}.png`} width={300} height={300} alt={oshi.oshikabu.name} />
@@ -63,11 +69,25 @@ export default function Result() {
           {oshi && <div className='w-full flex items-center justify-center'>
             <button 
               className="bg-blue-500 hover:bg-blue-700 disabled:bg-slate-300 mt-4 text-white font-bold py-2 px-8 rounded"
-              onClick={handleBuy}
+              onClick={handleOnClick}
+              disabled={pending}
             >
               買ってみたい
             </button>
           </div>}
+          <div className='max-w-full max-h-full'>
+            <button className='bg-black text-white text-xs rounded-lg p-1.5'>
+              <a
+                href={`https://x.com/intent/post?url=${process.env.NEXT_PUBLIC_BASE_URL}&hashtags=推し株診断&text=${twitterShareContext}`}
+                target="_blank"
+                rel="noopener noreferrer"
+                className='flex items-center'
+              >
+                <Image src="/x-logo.png" width={20} height={20} alt="X Logo" />
+                ポストする
+              </a>
+            </button>
+          </div>
           <p className='text-sm'>100円から株を買うことができます</p>
         </DialogDescription>
       </DialogContent>
